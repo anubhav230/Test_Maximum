e555e9ae803dea2e5e377c1ab23701159a0693cc
anubhav230 <anubhavsingh230@gmail.com>
2020-06-21 03:25:27 +0530

    ADDED-Method for finding max from array

--- src/main/java/com/bridgelabz/testmaximum/Maximum.java
+++ src/main/java/com/bridgelabz/testmaximum/Maximum.java
@@ -1,6 +1,17 @@
 package com.bridgelabz.testmaximum;
 
 public class Maximum {
-
-
+    /**
+     *
+     * @param maxInt
+     * @return
+     */
+    public Integer findMaximum(Integer[] maxInt ) {
+        Integer max = 0;
+        for (Integer index : maxInt) {
+            if (index.compareTo(max) > 0)
+                max = index;
+        }
+        return max;
+    }
 }
--- src/test/java/com/bridgelabz/testmaximum/test/MaximumTest.java
+++ src/test/java/com/bridgelabz/testmaximum/test/MaximumTest.java
@@ -1,7 +1,23 @@
 package com.bridgelabz.testmaximum.test;
 
 import com.bridgelabz.testmaximum.Maximum;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class MaximumTest {
+
+    @Test
+    public void givenInteger_WhenProper_ShouldReturnTrue() {
+        Maximum maximum = new Maximum();
+        Integer maxInt[] = {5, 8, 1};
+        Assert.assertEquals(maximum.findMaximum(maxInt),maxInt[1]);
+    }
+
+    @Test
+    public void givenInteger_WhenWrong_ShouldReturnFalse() {
+        Maximum maximum = new Maximum();
+        Integer maxInt[] = {10, 8, 1};
+        Assert.assertEquals(maximum.findMaximum(maxInt),maxInt[0]);
+    }
 
 }
