eb854cdca572a73b09509566e5256ba877cddf4f
anubhav230 <anubhavsingh230@gmail.com>
2020-06-21 16:08:20 +0530

    ADDED-method for find max string

--- src/test/java/com/bridgelabz/testmaximum/test/MaximumTest.java
+++ src/test/java/com/bridgelabz/testmaximum/test/MaximumTest.java
@@ -6,46 +6,62 @@ import org.junit.Test;
 
 public class MaximumTest {
 
+    Maximum maximum = new Maximum();
+
     @Test
-    public void givenInteger_WhenProper_ShouldReturnTrue() {
-        Maximum maximum = new Maximum();
-        Integer maxInt[] = {5, 8, 1};
-        Assert.assertEquals(maximum.findMaximum(maxInt),maxInt[1]);
+    public void givenMaxInteger_WhenFirstPosition_ShouldReturnMaxInt() {
+
+        int max = maximum.findMaximum(3,1,2);
+        Assert.assertEquals(3, max);
     }
 
     @Test
-    public void givenInteger_WhenProperSecond_ShouldReturnTrue() {
-        Maximum maximum = new Maximum();
-        Integer maxInt[] = {10, 8, 1};
-        Assert.assertEquals(maximum.findMaximum(maxInt),maxInt[0]);
+    public void givenMaxInteger_WhenSecondPosition_ShouldReturnMaxInt() {
+        int max = maximum.findMaximum(3,8,2);
+        Assert.assertEquals(8, max);
     }
 
     @Test
-    public void givenInteger_WhenProperThird_ShouldReturnTrue() {
-        Maximum maximum = new Maximum();
-        Integer maxInt[] = {23, 6,9};
-        Assert.assertEquals(maximum.findMaximum(maxInt),maxInt[0]);
+    public void givenMaxInteger_WhenThirdPosition_ShouldReturnMaxInt() {
+        int max = maximum.findMaximum(3,8,12);
+        Assert.assertEquals(12, max);
     }
 
     @Test
-    public void givenFloat_WhenProper_ShouldReturnTrue() {
-        Maximum maximum = new Maximum();
-        Float maxInt[] = {23.4f, 50.2f,9.5f};
-        Assert.assertEquals(maximum.findMaximum(maxInt),maxInt[1]);
+    public void givenMaxFloat_WhenFirstPosition_ShouldReturnMaxFloat() {
+
+        float max = maximum.findMaximum(51.4f, 50.2f,9.5f);
+        Assert.assertEquals(51.4f, max, 0.0f);
     }
 
     @Test
-    public void givenFloat_WhenProperTwo_ShouldReturnTrue() {
-        Maximum maximum = new Maximum();
-        Float maxInt[] = {2.3f, 2.2f,2.5f};
-        Assert.assertEquals(maximum.findMaximum(maxInt),maxInt[2]);
+    public void givenMaxFloat_WhenSecondPosition_ShouldReturnMaxFloat() {
+
+        float max = maximum.findMaximum(51.4f, 52.2f,9.5f);
+        Assert.assertEquals(52.2f, max, 0.0f);
     }
 
     @Test
-    public void givenFloat_WhenProperThird_ShouldReturnTrue() {
-        Maximum maximum = new Maximum();
-        Float maxInt[] = {0.9f, 0.2f,0.5f};
-        Assert.assertEquals(maximum.findMaximum(maxInt),maxInt[0]);
+    public void givenMaxFloat_WhenThirdPosition_ShouldReturnMaxFloat() {
+        float max = maximum.findMaximum(1.4f, 5.2f,9.5f);
+        Assert.assertEquals(9.5f, max, 0.0f);
     }
 
+    @Test
+    public void givenMaxString_WhenFirstPosition_ShouldReturnMaxString() {
+        String max = maximum.findMaximum("apple","Peach","Banana");
+        Assert.assertEquals("apple", max);
+    }
+
+    @Test
+    public void givenMaxString_WhenSecondPosition_ShouldReturnMaxString() {
+        String max = maximum.findMaximum("Apple","Peach","Banana");
+        Assert.assertEquals("Peach", max);
+    }
+
+    @Test
+    public void givenMaxString_WhenThirdPosition_ShouldReturnMaxString() {
+        String max = maximum.findMaximum("Apple","Peach","banana");
+        Assert.assertEquals("banana", max);
+    }
 }
