9a1772cb2171a89b2a25cde84f2d02e97c0d5ab5
anubhav230 <anubhavsingh230@gmail.com>
2020-06-21 20:44:28 +0530

    REFACTORED-Created generic class to take variables of generic type

--- src/main/java/com/bridgelabz/testmaximum/Maximum.java
+++ src/main/java/com/bridgelabz/testmaximum/Maximum.java
@@ -1,26 +1,35 @@
 package com.bridgelabz.testmaximum;
 
-public class Maximum {
-    /**
-     *
-     * @param maxInt
-     * @return
-     */
-    public Integer findMaximum(Integer[] maxInt ) {
-        Integer max = 0;
-        for (Integer index : maxInt) {
-            if (index.compareTo(max) > 0)
-                max = index;
-        }
-        return max;
+public class Maximum<E extends Comparable<E>> {
+    private E firstValue;
+    private E secondValue;
+    private E thirdValue;
+
+    public Maximum(E firstValue, E secondValue, E thirdValue) {
+        this.firstValue = firstValue;
+        this.secondValue = secondValue;
+        this.thirdValue = thirdValue;
     }
 
-    public Float findMaximum(Float[] maxInt ) {
-        Float max = 0f;
-        for (Float index : maxInt) {
-            if (index.compareTo(max) > 0)
-                max = index;
-        }
-        return max;
+    /**
+     * method to fid maximum value of generic type
+     *
+     * @param firstValue
+     * @param secondValue
+     * @param thirdValue
+     * @param <E>
+     * @return
+     */
+    public <E extends Comparable<E>> E findMaximum(E firstValue, E secondValue, E thirdValue) {
+        if (firstValue.compareTo(secondValue) > 0 && firstValue.compareTo(thirdValue) > 0)
+            return firstValue;
+        else if (secondValue.compareTo(thirdValue) > 0 && secondValue.compareTo(firstValue) > 0)
+            return secondValue;
+        else
+            return thirdValue;
+    }
+
+    public E findMaximum() {
+        return findMaximum(firstValue, secondValue, thirdValue);
     }
 }
--- src/test/java/com/bridgelabz/testmaximum/test/MaximumTest.java
+++ src/test/java/com/bridgelabz/testmaximum/test/MaximumTest.java
@@ -5,46 +5,68 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MaximumTest {
+
     @Test
-    public void givenInteger_WhenProper_ShouldReturnTrue() {
-        Maximum maximum = new Maximum();
-        Integer maxInt[] = {5, 8, 1};
-        Assert.assertEquals(maximum.findMaximum(maxInt),maxInt[1]);
+    public void givenMaxInteger_WhenFirstPosition_ShouldReturnMaxInt() {
+        Maximum maximum = new Maximum(6, 2, 1);
+        Comparable max = maximum.findMaximum();
+        Assert.assertEquals(6, max);
     }
 
     @Test
-    public void givenInteger_WhenProperSecond_ShouldReturnTrue() {
-        Maximum maximum = new Maximum();
-        Integer maxInt[] = {10, 8, 1};
-        Assert.assertEquals(maximum.findMaximum(maxInt),maxInt[0]);
+    public void givenMaxInteger_WhenSecondPosition_ShouldReturnMaxInt() {
+        Maximum maximum = new Maximum(6, 8, 1);
+        Comparable max = maximum.findMaximum();
+        Assert.assertEquals(8, max);
     }
 
     @Test
-    public void givenInteger_WhenProperThird_ShouldReturnTrue() {
-        Maximum maximum = new Maximum();
-        Integer maxInt[] = {23, 6,9};
-        Assert.assertEquals(maximum.findMaximum(maxInt),maxInt[0]);
+    public void givenMaxInteger_WhenThirdPosition_ShouldReturnMaxInt() {
+        Maximum maximum = new Maximum(6, 8, 9);
+        Comparable max = maximum.findMaximum();
+        Assert.assertEquals(9, max);
     }
 
     @Test
-    public void givenFloat_WhenProper_ShouldReturnTrue() {
-        Maximum maximum = new Maximum();
-        Float maxInt[] = {23.4f, 50.2f,9.5f};
-        Assert.assertEquals(maximum.findMaximum(maxInt),maxInt[1]);
+    public void givenMaxFloat_WhenFirstPosition_ShouldReturnMaxFloat() {
+
+        Maximum maximum = new Maximum(5.5f, 2.5f, 2.4f);
+        Comparable max = maximum.findMaximum();
+        Assert.assertEquals(5.5f, max);
     }
 
     @Test
-    public void givenFloat_WhenProperTwo_ShouldReturnTrue() {
-        Maximum maximum = new Maximum();
-        Float maxInt[] = {2.3f, 2.2f,2.5f};
-        Assert.assertEquals(maximum.findMaximum(maxInt),maxInt[2]);
+    public void givenMaxFloat_WhenSecondPosition_ShouldReturnMaxFloat() {
+        Maximum maximum = new Maximum(5.5f, 16.5f, 2.4f);
+        Comparable max = maximum.findMaximum();
+        Assert.assertEquals(16.5f, max);
     }
 
     @Test
-    public void givenFloat_WhenProperThird_ShouldReturnTrue() {
-        Maximum maximum = new Maximum();
-        Float maxInt[] = {0.9f, 0.2f,0.5f};
-        Assert.assertEquals(maximum.findMaximum(maxInt),maxInt[0]);
+    public void givenMaxFloat_WhenThirdPosition_ShouldReturnMaxFloat() {
+        Maximum maximum = new Maximum(5.5f, 16.5f, 23.4f);
+        Comparable max = maximum.findMaximum();
+        Assert.assertEquals(23.4f, max);
     }
 
+    @Test
+    public void givenMaxString_WhenFirstPosition_ShouldReturnMaxString() {
+        Maximum maximum = new Maximum("apple", "Peach", "Banana");
+        Comparable max = maximum.findMaximum();
+        Assert.assertEquals("apple", max);
+    }
+
+    @Test
+    public void givenMaxString_WhenSecondPosition_ShouldReturnMaxString() {
+        Maximum maximum = new Maximum("Apple", "Peach", "Banana");
+        Comparable max = maximum.findMaximum();
+        Assert.assertEquals("Peach", max);
+    }
+
+    @Test
+    public void givenMaxString_WhenThirdPosition_ShouldReturnMaxString() {
+        Maximum maximum = new Maximum("Apple", "Peach", "banana");
+        Comparable max = maximum.findMaximum();
+        Assert.assertEquals("banana", max);
+    }
 }
